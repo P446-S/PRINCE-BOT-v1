63506d979373ba1fdfb4b8508489e2f481c1c206
P446-S <princeuzochukwu41@gmail.com>
2026-01-11 11:34:37 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -4,7 +4,7 @@ export default {
   command: ["owner"],
   run: async ({ sock, msg }) => {
     await sock.sendMessage(msg.key.remoteJid, {
-      text: `👑 Owner: ${BOT_CONFIG.OWNER_NUMBER}`
+      text: `👑 Owner: THE PRINCE TECH DEV CONTACT HIM IN WHATSAP OR VIA TELEGRAM AT @Prince000r OR +2349033168045 A💀💀🚫🚫❤️${BOT_CONFIG.OWNER_NUMBER}`
     })
   }
   }
